a3470768e97513414f9883a84b1aa878b29db1f5
valen <festorazzivalentin9@gmail.com>
2026-09-17 15:21:49 -0300

    First MVP

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './CardComponent.js'; // ciclo de vida 4 - para que un webcomponent cargue, hay que importarlo en el index.js asociado al index.html
-import './WeatherTime.js'
-import './components/VerificationCard/VerificationCard.js';
-import './components/ActionBtn/ActionBtn.js';
-import './components/LinkBtn/LinkBtn.js';
-import './components/LogoBlueYellow/LogoBlueYellow.js';
-import './components/FlagLogo/FlagLogo.js';
-import './components/InputLabel/InputLabel.js';
