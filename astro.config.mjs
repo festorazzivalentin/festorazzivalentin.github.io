453cc9325eb1c505fc24bf6e9f2111eb4aebb6de
valen <festorazzivalentin9@gmail.com>
2026-09-18 19:04:14 -0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import supersvgPlugin from 'vite-plugin-supersvg';
 //docs.astro.build
 export default defineConfig({
     compressHTML: false, // comprime HTML
+    site: 'https://festorazzivalentin.github.io',
     vite: {
         plugins: [
             supersvgPlugin()
